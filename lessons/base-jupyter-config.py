9021ee95b1df669be756f976786fcbbb98fb3c4a
Matt Oswalt <matt@keepingitclassless.net>
2018-06-19 02:05:07 -0700

    Updated security policy
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- lessons/base-jupyter-config.py
+++ lessons/base-jupyter-config.py
@@ -7,4 +7,4 @@ c.NotebookApp.password = ''
 # c.NotebookApp.password_required = False
 
 # For embedding inside other pages
-c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors 'self' http://tf-controller01.labs.networkreliability.engineering" } }
+c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors 'self' http://tf-controller0.labs.networkreliability.engineering:9090/" } }
