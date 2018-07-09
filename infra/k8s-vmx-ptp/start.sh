a47a97845c6189972fbf9db2c139cac4bc10a516
Matt Oswalt <matt@keepingitclassless.net>
2018-07-09 13:32:25 -0700

    Get rid of old k8s lab attempts
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- infra/k8s-vmx-ptp/start.sh
+++
@@ -1,7 +0,0 @@
-kubectl create -f ptp-mgmt.yaml
-kubectl create -f ptp12.yaml
-kubectl create -f ptp23.yaml
-kubectl create -f ptp31.yaml
-kubectl create -f vmx1.yaml
-kubectl create -f vmx2.yaml
-kubectl create -f vmx3.yaml
--- infra/k8s-vmx-ptp/stop.sh
+++
@@ -1,3 +0,0 @@
-kubectl delete svc vmx1-service vmx2-service vmx3-service
-kubectl delete pod vmx1 vmx2 vmx3
-kubectl delete network ptp12 ptp23 ptp31 ptp-mgmt
