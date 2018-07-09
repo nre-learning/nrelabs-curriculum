0eea43dc32f941c53d237c363fec0b44200ccb50
Matt Oswalt <matt@keepingitclassless.net>
2018-07-09 13:32:47 -0700

    Simplify initial csrx lab
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- lessons/lesson-1/k8s-csrx-weave/start.sh
+++ lessons/lesson-1/k8s-csrx-weave/start.sh
@@ -1,9 +1,6 @@
 # This will eventually be replaced by a third-party tool
 
-kubectl create -f weave-mgmt.yaml
-kubectl create -f weave-12.yaml
-kubectl create -f weave-23.yaml
-kubectl create -f weave-31.yaml
+kubectl create -f weave-lab1.yaml
 sleep 5
 kubectl create -f csrx1.yaml
 kubectl create -f csrx2.yaml
--- lessons/lesson-1/k8s-csrx-weave/stop.sh
+++ lessons/lesson-1/k8s-csrx-weave/stop.sh
@@ -1,3 +1,3 @@
 kubectl delete svc csrx1-service csrx2-service csrx3-service
 kubectl delete pod csrx1 csrx2 csrx3
-kubectl delete network weave-mgmt weave-12 weave-23 weave-31 
+kubectl delete network weave-lab1 
