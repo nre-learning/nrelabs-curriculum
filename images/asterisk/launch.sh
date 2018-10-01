96cae52d926740b4158cb8e2b6ac203e991d24cb
Matt Oswalt <matt@keepingitclassless.net>
2018-10-01 12:44:53 -0700

    Updated images
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- images/asterisk/launch.sh
+++ images/asterisk/launch.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -d "/sys/devices/virtual/net/eth1" ]; then
-    ip address change 10.10.100.20/24 dev eth1
-    ip route add 10.0.0.0/8 via 10.10.100.1 dev eth1
-fi
+# if [ -d "/sys/devices/virtual/net/net1" ]; then
+#     ip address change 10.10.100.20/24 dev net1
+#     ip route add 10.0.0.0/8 via 10.10.100.1 dev net1
+# fi
+
+ip address change 10.10.100.20/24 dev net1
+ip route add 10.0.0.0/8 via 10.10.100.1 dev net1
 
 /usr/bin/ssh-keygen -A
 /usr/sbin/sshd
