dbab86918cdd4090adf13d68aca3c67cf5e2acb0
cloudtoad <ccie15672@gmail.com>
2018-10-04 18:33:56 -0500

    Updated subnets

--- images/asterisk/launch.sh
+++ images/asterisk/launch.sh
@@ -5,8 +5,9 @@
 #     ip route add 10.0.0.0/8 via 10.10.100.1 dev net1
 # fi
 
-ip address change 10.10.100.20/24 dev net1
-ip route add 10.0.0.0/8 via 10.10.100.1 dev net1
+ip address flush dev net1
+ip address add 10.10.200.10/24 dev net1
+ip route add 10.10.0.0/16 via 10.10.200.1 dev net1
 
 /usr/bin/ssh-keygen -A
 /usr/sbin/sshd
