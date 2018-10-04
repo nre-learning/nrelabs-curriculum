1af8bd25399b55bb8bcc98e28cb7400dc9f48e61
cloudtoad <ccie15672@gmail.com>
2018-10-04 18:33:56 -0500

    Updated subnets

--- images/netbox/launch.sh
+++ images/netbox/launch.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 
-ip address change 10.10.50.10/24 dev net1
-ip route add 10.0.0.0/8 via 10.10.50.1 dev net1
+ip address flush dev net1
+ip address add 10.10.300.10/24 dev net1
+ip route add 10.10.0.0/16 via 10.10.300.1 dev net1
 
 
 su - postgres -c "/usr/pgsql-9.6/bin/pg_ctl start -w -D /var/lib/pgsql/9.6/data"
