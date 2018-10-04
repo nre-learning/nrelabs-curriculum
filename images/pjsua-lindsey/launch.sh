b42db6a026e6c5a76d0ecc5d938193f04a6b4cae
cloudtoad <ccie15672@gmail.com>
2018-10-04 18:38:15 -0500

    Updated subnets

--- images/pjsua-lindsey/launch.sh
+++ images/pjsua-lindsey/launch.sh
@@ -10,6 +10,6 @@
 # sleep 1000
 
 ip address flush dev net1
-ip address add 10.10.300.10/24 dev net1
-ip route add 10.10.0.0/16 via 10.10.300.1 dev net1
+ip address add 10.10.150.10/24 dev net1
+ip route add 10.10.0.0/16 via 10.10.150.1 dev net1
 /root/pjproject-2.8/pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu --id sip:1107@10.10.100.20 --registrar sip:10.10.100.20 --realm * --username 0019159BF771 --password 4webrEtHupHewu4
