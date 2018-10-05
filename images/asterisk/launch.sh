7057844be6d4033d4cf07c3687625f6d5686f55d
cloudtoad <ccie15672@gmail.com>
2018-10-04 19:16:54 -0500

    Updated subnets

--- images/asterisk/launch.sh
+++ images/asterisk/launch.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# if [ -d "/sys/devices/virtual/net/net1" ]; then
-#     ip address change 10.10.100.20/24 dev net1
-#     ip route add 10.0.0.0/8 via 10.10.100.1 dev net1
-# fi
-
 ip address flush dev net1
 ip address add 10.10.200.10/24 dev net1
 ip route add 10.10.0.0/16 via 10.10.200.1 dev net1
