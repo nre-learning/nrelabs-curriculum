69f7df1c29de5418cbb369e92272122aee149dfa
cloudtoad <ccie15672@gmail.com>
2018-10-04 19:03:08 -0500

    Updated subnets

--- images/netbox/launch.sh
+++ images/netbox/launch.sh
@@ -2,12 +2,12 @@
 
 
 ip address flush dev net1
-ip address add 10.10.300.10/24 dev net1
-ip route add 10.10.0.0/16 via 10.10.300.1 dev net1
+ip address add 10.10.100.10/24 dev net1
+ip route add 10.10.0.0/16 via 10.10.100.1 dev net1
 
 
 su - postgres -c "/usr/pgsql-9.6/bin/pg_ctl start -w -D /var/lib/pgsql/9.6/data"
-cd cd /opt/netbox/netbox
+cd /opt/netbox/netbox
 python3 manage.py runserver 0.0.0.0:8000 --insecure
 
 
