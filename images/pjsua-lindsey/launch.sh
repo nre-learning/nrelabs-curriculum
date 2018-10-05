17c05343c0bc4fd9f0fac46a59d1143f6773cbf3
cloudtoad <ccie15672@gmail.com>
2018-10-04 21:38:41 -0500

    remove sip client for testing

--- images/pjsua-lindsey/launch.sh
+++ images/pjsua-lindsey/launch.sh
@@ -9,6 +9,5 @@ ip route add 10.10.0.0/16 via 10.10.150.1 dev net1
 echo "10.10.200.10  asterisk" >> /etc/hosts
 
 /usr/bin/ssh-keygen -A
-/usr/sbin/sshd
-/bin/sh
+/usr/sbin/sshd -D
 #/root/pjproject-2.8/pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu --id sip:1107@asterisk --registrar sip:asterisk --realm * --username 0019159BF771 --password 4webrEtHupHewu4
