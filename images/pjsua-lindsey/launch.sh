71148f75c67e59db86f0d73fc861acc2e6a90b89
cloudtoad <ccie15672@gmail.com>
2018-10-06 14:33:10 -0500

    added bgp config

--- images/pjsua-lindsey/launch.sh
+++ images/pjsua-lindsey/launch.sh
@@ -10,4 +10,5 @@ echo "10.10.200.10  asterisk" >> /etc/hosts
 
 /usr/bin/ssh-keygen -A
 /usr/sbin/sshd -D
-#/root/pjproject-2.8/pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu --id sip:1107@asterisk --registrar sip:asterisk --realm * --username 0019159BF771 --password 4webrEtHupHewu4
+
+/root/pjproject-2.8/pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu --id sip:1107@asterisk --registrar sip:asterisk  --realm asterisk --username 0019159BF771 --password 4webrEtHupHewu4 --bound-addr 10.10.150.10 --ip-addr 10.10.150.10
