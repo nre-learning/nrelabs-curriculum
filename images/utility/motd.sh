c91e18013f7a7a78453254dd818a811cded8ceee
Matt Oswalt <matt@keepingitclassless.net>
2018-10-06 18:39:23 -0700

    Disable root auth in utility
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

---
+++ images/utility/motd.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+printf "\n"
+printf " ###############################################################\n"
+printf " #                     WELCOME TO ANTIDOTE                     #\n"            
+printf " #                                                             #\n"
+printf " # This is the 'utility' container. This is used to run Python #\n"
+printf " # scripts, workflows, tools, commands, etc - anything needed  #\n"
+printf " # to perform automation tasks on our network devices.         #\n"
+printf " #                                                             #\n"
+printf " # Documentation: https://antidoteproject.readthedocs.io       #\n"
+printf " ###############################################################\n"
--- infrastructure/bootstrapcluster.sh
+++ infrastructure/bootstrapcluster.sh
@@ -3,3 +3,4 @@ rm -rf venv/ && virtualenv -p python3 venv && source venv/bin/activate
 pip3 install -r requirements.txt
 ansible-playbook -i inventory/ prepinstances.yml
 kubectl config set-cluster kubernetes --insecure-skip-tls-verify=true --server=https://$(gcloud compute instances list | grep controller | awk '{print $5}'):6443
+kubectl config set current-context kubernetes-admin@kubernetes
