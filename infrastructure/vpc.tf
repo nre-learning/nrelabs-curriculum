22869d4fc04cdd24f764af812c16f2a6add05829
Matt Oswalt <matt@keepingitclassless.net>
2018-10-06 19:35:38 -0700

    Make firewall rules a bit more legit
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,28 +1,57 @@
-resource "google_compute_firewall" "supersecure" {
-  name = "supersecure"
+resource "google_compute_firewall" "restrict-external" {
+  name = "restrict-external"
 
   project = "${var.project}"
-
-  # depends_on = [
-  #   "google_project_services.project",
-  # ]
 
   network = "${google_compute_network.default-internal.name}"
 
   allow {
-    protocol = "all"
+    protocol = "tcp"
+
+    # 3001 = nginx http nodeport
+    # 3002 = nginx https nodeport
+    # 6443 = k8s API
+    ports    = ["30001", "30002", "6443"]
   }
 
-  source_tags = []
+  # source_tags = []
+
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "google_compute_firewall" "allow-internal-antidote" {
+  name = "allow-internal-antidote"
+
+  project = "${var.project}"
+
+  network = "${google_compute_network.default-internal.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  // Default allow-all range in GCE isn't broad enough. I.e. instances are 10.128.0.0/9 but weave is 10.32.0.0/8 by default, etc etc
+  // Pretty much everything is under 10.0.0.0/8, and really, the main thing is to just apply to everything internal anyways. May as well
+  // expand to all RFC1918 tbh.
+  source_ranges = [
+    "10.0.0.0/8"
+  ]
 }
 
 resource "google_compute_network" "default-internal" {
   name = "default-internal"
-
-  # depends_on = [
-  #   "google_project_services.project",
-  # ]
-
   project                 = "${var.project}"
   auto_create_subnetworks = "true"
 }
