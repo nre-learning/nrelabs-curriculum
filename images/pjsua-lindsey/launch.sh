8ff5b977cd8aa04a98b7109f94d9086c34515054
cloudtoad <ccie15672@gmail.com>
2018-10-07 16:38:22 -0500

    make phone utility

--- images/pjsua-lindsey/launch.sh
+++ images/pjsua-lindsey/launch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 
-
 ip address flush dev net1
 ip address add 10.10.150.10/24 dev net1
 ip route add 10.10.0.0/16 via 10.10.150.1 dev net1
@@ -9,6 +8,5 @@ ip route add 10.10.0.0/16 via 10.10.150.1 dev net1
 echo "10.10.200.10  asterisk" >> /etc/hosts
 
 /usr/bin/ssh-keygen -A
-/usr/sbin/sshd
+/usr/sbin/sshd -D
 
-/root/pjproject-2.8/pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu --id sip:1107@asterisk --registrar sip:asterisk  --realm asterisk --username 0019159BF771 --password 4webrEtHupHewu4 --bound-addr 10.10.150.10 --ip-addr 10.10.150.10
