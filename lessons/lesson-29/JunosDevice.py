83b5e2d072808372cdb58f014122683d8b9029f0
Lakshmi Rajan <lara29.er@gmail.com>
2018-10-26 09:26:22 -0700

    Create JunosDevice.py

---
+++ lessons/lesson-29/JunosDevice.py
@@ -0,0 +1,31 @@
+from jnpr.junos import Device
+
+class JunosDevice(object):
+
+    def __init__(self):
+        self.device = None
+
+    def connect_device(self,host, user, password):
+        self.device = Device(host, user=user, password=password)
+        self.device.open()
+
+    def gather_device_info(self):
+        df = dict(self.device.facts)
+        device_facts =  {
+            "os_version": df["version"],
+            "serialnumber": df["serialnumber"],
+            "model": df["model"],
+            "hostname": df["hostname"]
+        }
+        return device_facts
+
+    def close_device(self):
+        self.device.close()
+
+    def get_hostname(self):
+        facts = self.gather_device_info()
+        return facts["hostname"]
+
+    def get_model(self):
+        facts = self.gather_device_info()
+        return facts["model"]
