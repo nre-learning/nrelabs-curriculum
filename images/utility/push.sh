2fbd838c90990d626ba978f6ec686422cf9a6fca
Lakshmi Rajan <lrajan@juniper.net>
2018-10-29 13:46:55 -0700

    using custom image

--- images/utility/push.sh
+++ images/utility/push.sh
@@ -1,2 +1,4 @@
-docker build -t antidotelabs/utility .
-docker push antidotelabs/utility
+#docker build -t antidotelabs/utility .
+#docker push antidotelabs/utility
+docker build -t la29ra/utility .
+docker push la29ra/utility
