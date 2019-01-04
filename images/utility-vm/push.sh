9927f17f9826d6ef0504bec81b8f95062a095b66
Matt Oswalt <matt@keepingitclassless.net>
2019-01-03 22:56:07 -0800

    cleanup
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- images/utility-vm/push.sh
+++ images/utility-vm/push.sh
@@ -1,10 +1,10 @@
 
-docker kill utility-vm
-docker build -t antidotelabs/utility-vm .
-docker run --name utility-vm --rm -d -p 5900:5900 -p 2022:22 -v /Users/mierdin/Code/GO/src/github.com/nre-learning/antidote:/antidote antidotelabs/utility-vm
-docker exec -it utility-vm /bin/bash
+# docker kill utility-vm
+# docker build -t antidotelabs/utility-vm .
+# docker run --name utility-vm --rm -d -p 5900:5900 -p 2022:22 -v /Users/mierdin/Code/GO/src/github.com/nre-learning/antidote:/antidote antidotelabs/utility-vm
+# docker exec -it utility-vm /bin/bash
 
-
-# docker push antidotelabs/utility-vm
+docker build -t antidotelabs/utility-vm 
+docker push antidotelabs/utility-vm
 
 
