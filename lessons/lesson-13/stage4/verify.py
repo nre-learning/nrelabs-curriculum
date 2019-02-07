0633a6d3c7286cd3d4a36af28b0be3b880b10248
Matt Oswalt <matt@keepingitclassless.net>
2019-02-07 11:15:35 -0800

    Refine objective and verification
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- lessons/lesson-13/stage4/verify.py
+++ lessons/lesson-13/stage4/verify.py
@@ -1,3 +1,5 @@
-import sys
-print("Foobar")
-sys.exit(0)
+import napalm
+driver = napalm.get_network_driver("junos")
+device = driver(hostname="vqfx1", username="antidote", password="antidotepassword")
+device.open()
+assert device.get_interfaces()['em1']['description'] == 'Hello, World!'
