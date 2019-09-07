de8ceb4d28a1a019d80e04990f0014c9446e58eb
Matt Oswalt <matt@keepingitclassless.net>
2019-09-06 22:40:00 -0700

    Created Kali and Wordpress images
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

---
+++ images/wordpress/start-wp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/etc/init.d/mysql start
+sleep 10
+mysql -u root -pjuniper2019 -e "CREATE DATABASE wordpress_db;"
+mysql -u root -pjuniper2019 -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'root'@'localhost' IDENTIFIED BY 'juniper2019';"
+mysql -u root -pjuniper2019 -e "FLUSH PRIVILEGES;"
+
+# https://indigotree.co.uk/automated-wordpress-installation-with-bash-wp-cli/
+cd /var/www/html/wordpress
+
+wp --allow-root db create
+wp --allow-root core install \
+    --url="http://localhost:8080/wordpress" \
+    --title="Hackathon2019" \
+    --admin_user="wordpress" \
+    --admin_password="Wordpress@2019" \
+    --admin_email="wordpress@lab.local"
+
+/usr/sbin/apache2ctl -DFOREGROUND
