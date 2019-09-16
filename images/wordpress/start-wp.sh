27ed2c0d57b2d3c6227e83e37c289a0696d895d6
Matt Oswalt <matt@keepingitclassless.net>
2019-09-16 14:56:34 -0700

    minor updates to images
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- images/wordpress/start-wp.sh
+++ images/wordpress/start-wp.sh
@@ -7,11 +7,11 @@ mysql -u root -pjuniper2019 -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'root'
 mysql -u root -pjuniper2019 -e "FLUSH PRIVILEGES;"
 
 # https://indigotree.co.uk/automated-wordpress-installation-with-bash-wp-cli/
-cd /var/www/html/wordpress
+cd /var/www/html
 
 wp --allow-root db create
 wp --allow-root core install \
-    --url="http://localhost:8080/wordpress" \
+    --url="http://localhost:8080/" \
     --title="Hackathon2019" \
     --admin_user="wordpress" \
     --admin_password="Wordpress@2019" \
