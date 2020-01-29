e0417a01658a2260d49f7950984ec1e473160d71
Matt Oswalt <matt@oswalt.dev>
2020-01-28 17:38:21 -0800

    Final touches
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- lessons/fundamentals/lesson-17-git/stage2/configs/linux1.py
+++ lessons/fundamentals/lesson-17-git/stage2/configs/linux1.py
@@ -12,19 +12,10 @@ def createSSHClient(server, port, user, password):
 
 ssh=createSSHClient(host,22,"antidote","antidotepassword")
 
-# scp=SCPClient(ssh.get_transport())
-
-# scp.put('/antidote/stage1/configs/leaf1/interfaces', '/home/antidote/interfaces')
-# scp.put('/antidote/stage1/configs/leaf1/daemons', '/home/antidote/daemons')
-# scp.put('/antidote/stage1/configs/leaf1/frr.conf', '/home/antidote/frr.conf')
-
+ssh.exec_command('rm -rf /home/antidote/myfirstrepo')
 ssh.exec_command('mkdir -p /home/antidote/myfirstrepo')
 ssh.exec_command('cd /home/antidote/myfirstrepo && git init')
 ssh.exec_command('cd /home/antidote/myfirstrepo && git config --global user.email "jane@nrelabs.io"')
 ssh.exec_command('cd /home/antidote/myfirstrepo && git config --global user.name "Jane Doe"')
 
-# scp.close()
 ssh.close()
-
-
-
