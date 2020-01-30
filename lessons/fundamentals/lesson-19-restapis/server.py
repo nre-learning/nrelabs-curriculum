40e2ad730ee678775fb33cdac9b4586498b39cc7
Matt Oswalt <matt@oswalt.dev>
2020-01-29 21:27:34 -0800

    Re-vamping stage 1 guide for rest API lesson
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- lessons/fundamentals/lesson-19-restapis/server.py
+++ lessons/fundamentals/lesson-19-restapis/server.py
@@ -15,9 +15,50 @@ switches = [
      'operating_system': 'Cumulus'}
 ]
 
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/switches', methods=['GET'])
+# Return all switches
+@app.route('/api/v1/switches/all', methods=['GET'])
 def api_all():
     return jsonify(switches)
 
-app.run()
+# Return one switch by lookup
+@app.route('/api/v1/switches', methods=['GET'])
+def api_id():
+    if 'name' not in request.args:
+        return "Error: No name field provided. Please specify a name.\n" , 400
+
+    results = []
+    for switch in switches:
+        if switch['name'] == request.args['name']:
+            results.append(switch)
+
+    if not results:
+        return "Error: switch not found\n", 404
+
+    return jsonify(results)
+
+@app.route('/api/v1/switches', methods=['POST'])
+def api_create():
+
+    for switch in switches:
+        if switch['name'] == request.json.get('name'):
+            return "Error: switch already in current list\n", 400
+    switches.append(request.json)
+    resp = jsonify(request.json)
+    resp.status_code = 201
+    return resp
+
+@app.route('/api/v1/switches', methods=['DELETE'])
+def api_delete():
+    if 'name' not in request.args:
+        return "Error: No name field provided. Please specify a name.\n"
+
+    for switch in list(switches):
+        if switch['name'] == request.args['name']:
+            switches.remove(switch)
+            break
+    else:
+        return "Error: switch not found in list\n", 404
+
+    return '', 204
+
+app.run(host='0.0.0.0')
