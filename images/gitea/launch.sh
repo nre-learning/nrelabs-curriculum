2ba821e5fb8087af29a56459af031d353388afbc
Matt Oswalt <matt@oswalt.dev>
2020-04-13 15:43:40 -0700

    Fix gitea image to run on git user
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- images/gitea/launch.sh
+++ images/gitea/launch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+chown git:git /data/git
+chown -R git:git /data/gitea
+
 # Create first/admin user. Use this to connect via API and perform configuration tasks
-gitea admin create-user --username nreadmin --password Password1! --email nreadmin@nrelabs.io --admin
+su -c '/app/gitea/gitea -c /data/gitea/conf/app.ini admin create-user --name nreadmin --password Password1! --email nreadmin@nrelabs.io --admin' git
 
 # Start Gitea (original CMD from Gitea's Dockerfile)
 /bin/s6-svscan /etc/s6
