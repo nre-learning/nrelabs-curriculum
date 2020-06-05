b4f7d21c8106593dd989538dadb40b52b1625ad7
cklewar <cklewar@juniper.net>
2020-06-05 17:34:53 +0200

    - Test ansible-tower image creation

--- images/ansible-tower/launch.sh
+++ images/ansible-tower/launch.sh
@@ -4,6 +4,6 @@ qemu-system-x86_64 \
 -smp 2 \
 -m 2048 \
 -hda /tmp/hda.qcow2 \
--netdev user,hostname=ansible-tower,hostfwd=tcp::22-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,id=net \
+-netdev user,hostname=ansible-tower,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:80,hostfwd=tcp::8443-:443,id=net \
 -device virtio-net-pci,netdev=net -vnc :0 \
 -serial stdio
