f861f863648ba93c1d7eb95dace04d157db61459
cklewar <cklewar@juniper.net>
2020-06-05 20:42:10 +0200

    - add Makefile

--- images/ansible-tower/launch.sh
+++ images/ansible-tower/launch.sh
@@ -3,7 +3,7 @@ qemu-system-x86_64 \
 -enable-kvm \
 -smp 2 \
 -m 2048 \
--hda /tmp/hda.qcow2 \
+-hda /output/build/centos-8-amd64.qcow2 \
 -netdev user,hostname=ansible-tower,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:80,hostfwd=tcp::8443-:443,id=net \
 -device virtio-net-pci,netdev=net -vnc :0 \
 -serial stdio
