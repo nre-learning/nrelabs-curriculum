911b2a922bf3e6bbe94648a6a6cdea2a93437bfa
cklewar <cklewar@juniper.net>
2020-06-08 19:08:10 +0200

    - add Makefile

--- images/ansible-tower/launch.sh
+++ images/ansible-tower/launch.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
+random_mac () {
+    hexchars="0123456789abcdef"
+    end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
+
+    # QEMU OUI space - important to use this
+    echo 52:54:00$end
+}
+
+
 qemu-system-x86_64 \
 -enable-kvm \
 -smp 2 \
 -m 2048 \
 -hda /output/build/centos-8-amd64.qcow2 \
--netdev user,hostname=ansible-tower,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:80,hostfwd=tcp::8443-:443,id=net \
+-netdev user,hostname=ansible-tower,hostfwd=tcp::22-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,id=net \
 -device virtio-net-pci,netdev=net -vnc :0 \
 -serial stdio
