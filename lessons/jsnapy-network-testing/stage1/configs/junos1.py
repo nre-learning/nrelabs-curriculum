d3ba1494359d01e398737fc3df1c7c822c40f956
Matt Oswalt <matt@oswalt.dev>
2020-06-30 14:47:40 -0700

    Another config update
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- lessons/jsnapy-network-testing/stage1/configs/junos1.py
+++ lessons/jsnapy-network-testing/stage1/configs/junos1.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import napalm
 from jinja2 import FileSystemLoader, Environment
 
-user = sys.argv[1]
-password = sys.argv[2]
-vendor = sys.argv[3]
-port = sys.argv[4]
-host = sys.argv[5]
-template_file = sys.argv[6]
+user = "root"
+password = "antidotepassword"
+vendor = "junos"
+port = 22
+host = os.environ['ANTIDOTE_TARGET_HOST']
+config_file = "/antidote/stage1/configs/junos1.txt"
 
 driver = napalm.get_network_driver(vendor)
 with driver(hostname=host, username=user, password=password, optional_args={'port': port}) as device:
-    device.load_merge_candidate(filename=template_file)
+    device.load_merge_candidate(filename=config_file)
     device.commit_config()
-
