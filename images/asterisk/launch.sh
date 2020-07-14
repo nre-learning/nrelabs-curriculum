abd91861dc024f96943c919175dc185c26ff3e89
Matt Oswalt <matt@oswalt.dev>
2020-07-14 11:46:26 -0700

    Adjusting troubleshooting lesson post runtime changes
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- images/asterisk/launch.sh
+++ images/asterisk/launch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ip address flush dev net1
-ip address add 10.10.200.10/24 dev net1
-ip route add 10.10.0.0/16 via 10.10.200.1 dev net1
+# ip address flush dev net1
+# ip address add 10.10.200.10/24 dev net1
+# ip route add 10.10.0.0/16 via 10.10.200.1 dev net1
 
 /usr/bin/ssh-keygen -A
 /usr/sbin/sshd
--- images/pjsua-lindsey/launch.sh
+++ images/pjsua-lindsey/launch.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-
-ip address flush dev net1
-ip address add 10.10.150.10/24 dev net1
-ip route add 10.10.0.0/16 via 10.10.150.1 dev net1
-
-echo "10.10.200.10  asterisk" >> /etc/hosts
-
 /usr/bin/ssh-keygen -A
 /usr/sbin/sshd -D
 
