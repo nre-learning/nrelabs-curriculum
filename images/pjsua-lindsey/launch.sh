96163bcbc3fe2a0b3bcdbb6bfbc2e0ceae6193e8
Matt Oswalt <matt@oswalt.dev>
2020-07-15 13:14:03 -0700

    Set correct network settings for sip phone and asterisk, and re-order endpoints in lesson def
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- images/pjsua-lindsey/launch.sh
+++ images/pjsua-lindsey/launch.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
 
-# ip address flush dev net1
-# ip address add 10.10.150.10/24 dev net1
+ip address flush dev net1
+ip address add 10.10.150.10/24 dev net1
+ip route add 10.10.0.0/16 via 10.10.150.2 dev net1
 
-# TODO(mierdin): This is all VERY specific to the troubleshooting lesson, which is the only lesson that uses this image.
-# Ideally we'd perform this via some kind of runtime configuration which would need to be run as root, but currently the user
-# interacts as "antidote" user.
-ip route add 10.10.0.0/16 via 10.10.150.3 dev net1
-
-# echo "10.10.200.10  asterisk" >> /etc/hosts
+echo "10.10.200.10  asterisk" >> /etc/hosts
 
 /usr/bin/ssh-keygen -A
 /usr/sbin/sshd -D
