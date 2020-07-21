1fba1250b807843e6f52c0be2ddbc064fc0d02ec
Matt Oswalt <matt@oswalt.dev>
2020-07-21 10:58:29 -0700

    Remove screen from st2 services script
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- images/stackstorm/start_st2_services.sh
+++ images/stackstorm/start_st2_services.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-screen -d -t mongodb -m /usr/bin/mongod
-screen -d -t rabbitmq -m rabbitmq-server
+/usr/bin/mongod &
+rabbitmq-server &
 sleep 10
-screen -d -t st2actionrunner -m /opt/stackstorm/st2/bin/st2actionrunner --config-file=/etc/st2/st2.conf
-screen -d -t st2api -m /opt/stackstorm/st2/bin/st2api --config-file=/etc/st2/st2.conf
-screen -d -t st2auth -m /opt/stackstorm/st2/bin/st2auth --config-file=/etc/st2/st2.conf
-screen -d -t st2notifier -m /opt/stackstorm/st2/bin/st2notifier --config-file=/etc/st2/st2.conf
-screen -d -t st2rulesengine -m /opt/stackstorm/st2/bin/st2rulesengine --config-file=/etc/st2/st2.conf
-screen -d -t st2scheduler -m /opt/stackstorm/st2/bin/st2scheduler --config-file=/etc/st2/st2.conf
-screen -d -t st2sensorcontainer -m /opt/stackstorm/st2/bin/st2sensorcontainer --config-file=/etc/st2/st2.conf
-screen -d -t st2stream -m /opt/stackstorm/st2/bin/st2stream --config-file=/etc/st2/st2.conf
-screen -d -t st2timersengine -m /opt/stackstorm/st2/bin/st2timersengine --config-file=/etc/st2/st2.conf
-screen -d -t st2workflowengine -m /opt/stackstorm/st2/bin/st2workflowengine --config-file=/etc/st2/st2.conf
+/opt/stackstorm/st2/bin/st2actionrunner --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2api --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2auth --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2notifier --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2rulesengine --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2scheduler --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2sensorcontainer --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2stream --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2timersengine --config-file=/etc/st2/st2.conf &
+/opt/stackstorm/st2/bin/st2workflowengine --config-file=/etc/st2/st2.conf &
 
 /usr/sbin/sshd -D
