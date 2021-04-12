1107f1cb8cfccde7301055d0421fb6ac3478ebab
Matt Oswalt <matt@oswalt.dev>
2021-04-11 21:29:59 -0400

    Update WF
    Signed-off-by: Matt Oswalt <matt@oswalt.dev>

--- .scripts/prune-preview-tags.sh
+++ .scripts/prune-preview-tags.sh
@@ -2,7 +2,9 @@
 
 set -e +o pipefail
 
-wget -q "https://github.com/nre-learning/docker-housekeeping/releases/download/v0.1.0/docker-housekeeping-linux-amd64" && chmod +x docker-housekeeping-linux-amd64 && mv docker-housekeeping-linux-amd64 docker-housekeeping
+wget "https://github.com/nre-learning/docker-housekeeping/releases/download/v0.1.0/docker-housekeeping-linux-amd64"
+chmod +x docker-housekeeping-linux-amd64
+mv docker-housekeeping-linux-amd64 docker-housekeeping
 
 $(pwd)/docker-housekeeping prune-preview-tags
 
